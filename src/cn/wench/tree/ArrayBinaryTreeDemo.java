23a91fdc518ba5fc675b8d96fe722ea30fe32533
Wench98 <627042794@qq.com>
2020-06-07 22:47:40 +0800

    顺序存储二叉树

---
+++ src/cn/wench/tree/ArrayBinaryTreeDemo.java
@@ -0,0 +1,88 @@
+package cn.wench.tree;
+
+public class ArrayBinaryTreeDemo {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        //创建一个ArrayBinaryTree
+        ArrayBinaryTree arrayBinaryTree = new ArrayBinaryTree(arr);
+        arrayBinaryTree.preOrder();
+    }
+}
+
+//编写一个ArrayBinaryTree，实现顺序存储二叉树遍历
+class ArrayBinaryTree {
+    private int[] arr;  //存储数据节点的数组
+
+    public ArrayBinaryTree(int[] arr) {
+        this.arr = arr;
+    }
+
+    //重载
+    public void preOrder(){
+        this.preOrder(0);
+    }
+
+    public void infixOrder(){
+        this.infixOrder(0);
+    }
+
+    public void postOrder(){
+        this.postOrder(0);
+    }
+
+    //编写一个方法，完成顺序存储二叉树的前序遍历
+
+    /**
+     * @param index 数组的下标
+     */
+    public void preOrder(int index) {
+        //如果数组为空，或者arr.length == 0
+        if (arr == null || arr.length == 0) {
+            System.out.println("数组为空，不能进行二叉树的前序遍历");
+        }
+        //输出当前这个元素
+        System.out.println(arr[index]);
+        //向左递归遍历
+        if ((index * 2 + 1) < arr.length) {
+            preOrder(2 * index + 1);
+        }
+        //向右递归遍历
+        if ((index * 2 + 2) < arr.length) {
+            preOrder(2 * index + 2);
+        }
+    }
+
+    public void infixOrder(int index) {
+        //如果数组为空，或者arr.length == 0
+        if (arr == null || arr.length == 0) {
+            System.out.println("数组为空，不能进行二叉树的前序遍历");
+        }
+        //向左递归遍历
+        if ((index * 2 + 1) < arr.length) {
+            preOrder(2 * index + 1);
+        }
+        //输出当前这个元素
+        System.out.println(arr[index]);
+        //向右递归遍历
+        if ((index * 2 + 2) < arr.length) {
+            preOrder(2 * index + 2);
+        }
+    }
+
+    public void postOrder(int index) {
+        //如果数组为空，或者arr.length == 0
+        if (arr == null || arr.length == 0) {
+            System.out.println("数组为空，不能进行二叉树的前序遍历");
+        }
+        //向左递归遍历
+        if ((index * 2 + 1) < arr.length) {
+            preOrder(2 * index + 1);
+        }
+        //向右递归遍历
+        if ((index * 2 + 2) < arr.length) {
+            preOrder(2 * index + 2);
+        }
+        //输出当前这个元素
+        System.out.println(arr[index]);
+    }
+}
