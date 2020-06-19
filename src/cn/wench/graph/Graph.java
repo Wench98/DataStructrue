2990681ac15dbf9f5a2d37c9455c334d4e0afd77
Wench98 <627042794@qq.com>
2020-06-19 13:22:33 +0800

    深度优先搜索实现

--- src/cn/wench/graph/Graph.java
+++ src/cn/wench/graph/Graph.java
@@ -8,6 +8,8 @@ public class Graph {
     private ArrayList<String> vertexList;   //存储顶点集合
     private int[][] edges;      //存储图对应的邻接矩阵
     private int numOfEdges;     //表示边的数目
+    //定义一个数组boolean[]，记录某个结点是否被访问
+    private boolean[] isVisited;
 
     public static void main(String[] args) {
         //测试图是否创建成功
@@ -20,13 +22,18 @@ public class Graph {
             graph.insertVertex(vertex);
         }
         //添加边
-        graph.insertEdge(0,1,1);
-        graph.insertEdge(0,2,1);
-        graph.insertEdge(1,2,1);
-        graph.insertEdge(1,3,1);
-        graph.insertEdge(1,4,1);
+        graph.insertEdge(0, 1, 1);
+        graph.insertEdge(0, 2, 1);
+        graph.insertEdge(1, 2, 1);
+        graph.insertEdge(1, 3, 1);
+        graph.insertEdge(1, 4, 1);
         //显示邻接矩阵
         graph.showGraph();
+
+        //测试dfs遍历是否可行
+        System.out.println("深度优先遍历");
+        graph.dfs();
+
     }
 
     //构造器
@@ -35,8 +42,66 @@ public class Graph {
         edges = new int[n][n];
         vertexList = new ArrayList<>(n);
         numOfEdges = 0;
+        isVisited = new boolean[5];
     }
 
+    /**
+     * 得到第一个邻接结点的下标w
+     *
+     * @param index
+     * @return 如果存在就返回对应的下标，否则返回-1
+     */
+    public int getFirstNeighbor(int index) {
+        for (int j = 0; j < vertexList.size(); j++) {
+            if (edges[index][j] > 0) {
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    public int getNextNeighbor(int v1, int v2) {
+        for (int j = v2 + 1; j < vertexList.size(); j++) {
+            if (edges[v1][j] > 0) {
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 深度优先算法
+     *
+     * @param isVisited
+     * @param i         第一次就是0
+     */
+    private void dfs(boolean[] isVisited, int i) {
+        //首先访问该结点，输出
+        System.out.print(getValueByIndex(i) + "->");
+        //将结点设置为已经访问
+        isVisited[i] = true;
+        //查找结点i的第一个邻接结点w
+        int w = getFirstNeighbor(i);
+        while (w != -1) {//说明有
+            if (!isVisited[w]) {
+                dfs(isVisited, w);
+            }
+            //如果w结点已经被访问过
+            w = getNextNeighbor(i, w);
+        }
+    }
+
+    /**
+     * 对dfs进行一个重载，遍历所有的结点，并进行dfs
+     */
+    public void dfs() {
+        //遍历所有的结点，进行dfs[回溯]
+        for (int i = 0; i < getNumOfVertex(); i++) {
+            if (!isVisited[i]) {
+                dfs(isVisited, i);
+            }
+        }
+    }
 
     //图中常用的方法
 
